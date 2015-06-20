d59891e4ce84ad9f2c061954eba98c06dc543a38
Jerome Chenette & Paul Ozag <pair+jeromechenette.paulozag@devbootcamp.com>
2015-06-20 11:50:55 -0700

    woops, forgot the projects routes

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+  def new
+    # this takes us to a form page that allows a user to create a  new project
+  end
+
+  def create
+    # pulls from params the information for creating a new project - and creates the project
+  end
+
+  def edit
+    # this takes us to a form page that allows a user to edit an existing project
+  end
+
+  def update
+    # performs the update
+  end
+
+  def index
+    # this displays all projects
+  end
+
+  def show
+    # this displays a single project
+  end
+
+  def destroy
+    # this is the method that deletes an project
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     end
     resources :resources
     resources :events
+    resources :projects
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
