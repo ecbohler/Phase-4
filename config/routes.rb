238bcaacf0f36061d09bd3c2c3501c0bbe9e1f5c
Jerome Chenette & Paul Ozag <pair+jeromechenette.paulozag@devbootcamp.com>
2015-06-20 11:30:33 -0700

    completes the CRUD routes and associated erbs - still need to complete the migration columns for the db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :sessions, :only => [:new, :create]
   resources :users do
-    resources :challenges
+    resources :challenges do
+      resources :solutions
+    end
     resources :resources
+    resources :events
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
